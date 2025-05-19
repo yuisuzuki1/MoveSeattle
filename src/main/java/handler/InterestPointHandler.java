9d6a9d735058b0e67749f13420ba737a17f1675e
yuisuzuki1 <72286814+yuisuzuki1@users.noreply.github.com>
2025-05-18 17:32:19 -0700

    Update InterestPointHandler.java

--- src/main/java/handler/InterestPointHandler.java
+++ src/main/java/handler/InterestPointHandler.java
@@ -6,7 +6,7 @@ import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 
-public class HelloHttpFunction implements HttpFunction {
+public class InterestPointHandler implements HttpFunction {
   public void service(final HttpRequest request, final HttpResponse response) throws Exception {
     final BufferedWriter writer = response.getWriter();
     writer.write("Hello world!");
